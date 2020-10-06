fb8468cdbef1192214d3ea429714b012cfaa6386
Alex <format37@gmail.com>
2020-10-06 13:20:06 +0300

    modified:   queue_daemon.py

--- queue_daemon.py
+++ queue_daemon.py
@@ -71,7 +71,7 @@ def shortest_queue_cpu(conn, settings):
 		else:
 			sql_query += 'union all select '+str(i)+',0 '
 	sql_query += 'union all	select cpu_id, count(filename) from queue group by cpu_id; '
-	sql_query += 'select top 1 cpu_id, max(files_count)  from #tmp_cpu_queue_len group by cpu_id order by max(files_count);'	
+	sql_query += 'select top 1 cpu_id, max(files_count)  from #tmp_cpu_queue_len group by cpu_id order by max(files_count), cpu_id;'	
 	#print(sql_query) # debug
 	#return 0
 	cursor.execute(sql_query)
