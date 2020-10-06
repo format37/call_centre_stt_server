b88a0899bc7df649f832ccd0f748643a8a16cc9b
Alex <format37@gmail.com>
2020-10-06 09:28:30 +0300

    modified:   queue_daemon.py

--- queue_daemon.py
+++ queue_daemon.py
@@ -19,7 +19,7 @@ date_y = datetime.datetime.today().strftime('%Y')
 date_m = datetime.datetime.today().strftime('%m')
 date_d = datetime.datetime.today().strftime('%d')
 
-today_path = settings.audio_storage_path + settings.audio_path_prefix + date_y + '-' + date_m + '/'	date_d + '/'	
+today_path = settings.audio_storage_path + settings.audio_path_prefix + date_y + '-' + date_m + '/'	+ date_d + '/'	
 files_list = []
 for (dirpath, dirnames, filenames) in walk(today_path):
     files_list.extend(filenames)
