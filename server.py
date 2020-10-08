c8a719eacd19aff7dff918c5ab7c369a7f420f74
Alex <format37@gmail.com>
2020-10-08 18:01:40 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from init_server import stt_server
 
 server_object = stt_server(sys.argv[1])
 cursor = server_object.conn.cursor()
-sql_query =		"select filepath, filename, date_y, date_m, date_d from queue where cpu_id='"+server_object.cpu_id+"' order by date;"
+sql_query =		"select filepath, filename, date_y, date_m, date_d, duration from queue where cpu_id='"+server_object.cpu_id+"' order by date;"
 
 while True:
 	cursor.execute(sql_query)
@@ -16,14 +16,23 @@ while True:
 		server_object.date_y				= row[2]
 		server_object.date_m				= row[3]
 		server_object.date_d				= row[4]
+		server_object.original_file_duration= row[5]
 		
-		if server_object.make_file_splitted(0):
-			server_object.transcribe_to_sql(0)
-			server_object.remove_temporary_file()
+		if server_object.original_file_duration>5:
+		
+			if server_object.make_file_splitted(0):
+				server_object.transcribe_to_sql(0)
+				server_object.remove_temporary_file()
+
+			if server_object.make_file_splitted(1):
+				server_object.transcribe_to_sql(1)
+				server_object.remove_temporary_file()			
 			
-		if server_object.make_file_splitted(1):
-			server_object.transcribe_to_sql(1)
-			server_object.remove_temporary_file()
+		else:
+			
+			trans_date = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+			server_object.save_result('', '0', 0, trans_date)
+			server_object.save_result('', '0', 1, trans_date)
 			
 		server_object.delete_current_queue()
 			
