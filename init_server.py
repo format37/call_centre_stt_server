5fccab1193003d5b547087ef33a7b1c69d58a470
Alex <format37@gmail.com>
2020-10-08 14:22:28 +0300

    modified:   init_server.py

--- init_server.py
+++ init_server.py
@@ -135,7 +135,7 @@ class stt_server:
 			
 	def remove_temporary_file(self):
 		print('removing',self.temp_file_path + self.temp_file_name)
-		#os.remove(self.temp_file_path + self.temp_file_name)
+		os.remove(self.temp_file_path + self.temp_file_name)
 		
 	def get_sql_complete_files(self):
 	
