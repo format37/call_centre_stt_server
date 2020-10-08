81ac2db8b5f3de9dc695b684358f202f81de82b3
Alex <format37@gmail.com>
2020-10-08 14:34:28 +0300

    modified:   queue_daemon.py

--- queue_daemon.py
+++ queue_daemon.py
@@ -1,7 +1,7 @@
 import datetime
 import time
 from os import walk
-#from init_server import server_settings
+from init_server import stt_server
 #from init_server import connect_sql
 #from init_server import get_today_ymd
 	
