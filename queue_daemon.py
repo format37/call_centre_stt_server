62e98a0913dc55d9bc5e1ab661fbe1daddc65969
Alex <format37@gmail.com>
2020-10-09 09:01:02 +0300

    modified:   queue_daemon.py

--- queue_daemon.py
+++ queue_daemon.py
@@ -6,18 +6,20 @@ server_object = stt_server(0)
 #while True:
 	
 server_object.set_today_ymd()
-#complete_files	= server_object.get_sql_complete_files()
+complete_files	= server_object.get_sql_complete_files()
 cpu_id = 0
+files_count = 0
 for filename in server_object.get_fs_files_list():	
-	#	if not filename in complete_files:
-	#server_object.set_shortest_queue_cpu()
-	server_object.cpu_id = cpu_id
-	server_object.original_file_name = filename
-	server_object.add_queue()
-	print(cpu_id,filename)
-	cpu_id += 1
-	if cpu_id>len(server_object.cpu_cores)-1:
-		cpu_id = 0
+	if not filename in complete_files:
+		#server_object.set_shortest_queue_cpu()
+		server_object.cpu_id = cpu_id
+		server_object.original_file_name = filename
+		#server_object.add_queue()
+		print(files_count,cpu_id,filename)
+		cpu_id += 1
+		if cpu_id>len(server_object.cpu_cores)-1:
+			cpu_id = 0
+		files_count+=1
 
 #print(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'sleeping 10s..')
 #time.sleep(10)
