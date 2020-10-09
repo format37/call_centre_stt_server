65c8df1052292ac960ab866a5c51f3640f6a81fa
Alex <format37@gmail.com>
2020-10-09 10:26:26 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -33,8 +33,8 @@ while True:
 				files_converted+=1
 				
 			if files_converted==0:
-				print(server_object.cpu_id,datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'No converted files found. exit..')
-				exit();
+				#print(server_object.cpu_id,datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'No converted files found. exit..')
+				sys.exit(server_object.cpu_id,datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'No converted files found. exit..')
 			
 		else:
 			
