b259bfd73e070e358aa30f10d67006e11b920ebb
Alex <format37@gmail.com>
2020-10-09 08:35:09 +0300

    modified:   queue_daemon.py

--- queue_daemon.py
+++ queue_daemon.py
@@ -14,6 +14,7 @@ for filename in server_object.get_fs_files_list():
 	server_object.cpu_id = cpu_id
 	server_object.original_file_name = filename
 	server_object.add_queue()
+	print(cpu_id,filename)
 	cpu_id += 1
 	if cpu_id>len(server_object.cpu_cores)-1:
 		cpu_id = 0
