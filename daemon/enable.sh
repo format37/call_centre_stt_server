c8c5de6b362e6e9d8f694d9b9b673191944dd057
Alex format37 <format37@gmail.com>
2020-10-20 17:18:14 +0300

    new file:   daemon/edit_0.sh 	new file:   daemon/edit_1.sh 	new file:   daemon/edit_2.sh 	new file:   daemon/edit_queue.sh 	new file:   daemon/edit_sentiment.sh 	new file:   daemon/edit_sentiment.txt 	new file:   daemon/enable.sh 	new file:   daemon/reload.sh 	new file:   daemon/restart.sh 	new file:   daemon/stop.sh

---
+++ daemon/enable.sh
@@ -0,0 +1,5 @@
+sudo systemctl enable call_stt_0.service
+sudo systemctl enable call_stt_1.service
+sudo systemctl enable call_stt_2.service
+sudo systemctl enable call_stt_queue.service
+sudo systemctl enable call_stt_sentiment.service
---
+++ daemon/stop.sh
@@ -0,0 +1,5 @@
+sudo systemctl stop call_stt_0.service
+sudo systemctl stop call_stt_1.service
+sudo systemctl stop call_stt_2.service
+sudo systemctl stop call_stt_queue.service
+sudo systemctl stop call_stt_sentiment.service
