f762fa80650f2614812444783a27daf70f27350d
Alex <format37@gmail.com>
2020-10-20 15:35:39 +0300

    modified:   stt_sentiment.py

--- stt_sentiment.py
+++ stt_sentiment.py
@@ -6,20 +6,20 @@ import pandas as pd
 from init_server import stt_server
 
 BATCH_SIZE = 3
-	
+
 #"update transcribations set sentiment = 'test', sentiment_pos = 0, sentiment_neg = 1 where audio_file_name='in_9163652085_2020-10-09-07-11-39rxtx.wav' and side = 0 and start = 10.83 and transcribation_date='2020-10-09 08:36:55':
 #"select top 10 * from transcribations where sentiment is NULL and text!=''"
 
 server_object = stt_server(0)
 
 query = """
-    select top """+str(BATCH_SIZE)+""" 
-    id,
-    text,
-    sentiment
-    from transcribations 
-    where sentiment is NULL and text!=''
-    """
+	select top """+str(BATCH_SIZE)+""" 
+	id,
+	text,
+	sentiment
+	from transcribations 
+	where sentiment is NULL and text!=''
+	"""
 
 print('solving..')
 df = pd.read_sql(query, server_object.conn)
@@ -28,17 +28,17 @@ res = model(df.text)
 df['sentiment'] = model(df.text)
 
 for index, row in df.iterrows():
-    if row.sentiment == 'negative':
-        neg = 1
-        pos = 0
-    else:
-        neg = 0
-        pos = 1
-    query = "update transcribations set "
-    query += "sentiment = '"+row.sentiment+"', "
-    query += "sentiment_neg = "+str(neg)+", "
-    query += "sentiment_pos = "+str(pos)+" "
-    query += "where id = "+str(row.id)
+	if row.sentiment == 'negative':
+		neg = 1
+		pos = 0
+	else:
+		neg = 0
+		pos = 1
+	query = "update transcribations set "
+	query += "sentiment = '"+row.sentiment+"', "
+	query += "sentiment_neg = "+str(neg)+", "
+	query += "sentiment_pos = "+str(pos)+" "
+	query += "where id = "+str(row.id)
 	print(query)
 
 print('Happy end! exit..')
