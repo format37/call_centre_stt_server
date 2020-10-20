5332df77083ba94e8b189932664395de45a18b01
Alex <format37@gmail.com>
2020-10-20 15:46:14 +0300

    modified:   stt_sentiment.py

--- stt_sentiment.py
+++ stt_sentiment.py
@@ -5,20 +5,42 @@ from deeppavlov import build_model, configs
 import pandas as pd
 from init_server import stt_server
 
-BATCH_SIZE = 3
+BATCH_SIZE = 10
 
 #"update transcribations set sentiment = 'test', sentiment_pos = 0, sentiment_neg = 1 where audio_file_name='in_9163652085_2020-10-09-07-11-39rxtx.wav' and side = 0 and start = 10.83 and transcribation_date='2020-10-09 08:36:55':
 #"select top 10 * from transcribations where sentiment is NULL and text!=''"
+
+def update_record(server_object, df):
+	
+	query = ''
+	
+	for index, row in df.iterrows():
+		if row.sentiment == 'negative':
+			neg = 1
+			pos = 0
+		else:
+			neg = 0
+			pos = 1
+		query += "update transcribations set "
+		query += "sentiment = '"+row.sentiment+"', "
+		query += "sentiment_neg = "+str(neg)+", "
+		query += "sentiment_pos = "+str(pos)+" "
+		query += "where id = "+str(row.id)+";"
+	
+	print(query)
+	cursor = server_object.conn.cursor()
+	cursor.execute(query)
+	server_object.conn.commit()
+	
 
 server_object = stt_server(0)
 
 query = """
 	select top """+str(BATCH_SIZE)+""" 
-	id,
-	text,
-	sentiment
+	id,	text, sentiment
 	from transcribations 
 	where sentiment is NULL and text!=''
+	order by transcribation_date, start
 	"""
 
 print('solving..')
@@ -27,18 +49,6 @@ model = build_model(configs.classifiers.rusentiment_bert, download=False) #downl
 res = model(df.text)
 df['sentiment'] = model(df.text)
 
-for index, row in df.iterrows():
-	if row.sentiment == 'negative':
-		neg = 1
-		pos = 0
-	else:
-		neg = 0
-		pos = 1
-	query = "update transcribations set "
-	query += "sentiment = '"+row.sentiment+"', "
-	query += "sentiment_neg = "+str(neg)+", "
-	query += "sentiment_pos = "+str(pos)+" "
-	query += "where id = "+str(row.id)
-	print(query)
+update_record(df)
 
 print('Happy end! exit..')
