f969fb5b6320a76f0426e78f4d02ae9ff7413e7a
Alex <format37@gmail.com>
2020-10-27 07:50:19 +0300

    modified:   stt_sentiment.py

--- stt_sentiment.py
+++ stt_sentiment.py
@@ -5,8 +5,23 @@ from deeppavlov import build_model, configs
 import pandas as pd
 from init_server import stt_server
 import time
+import requests
+import datetime
 
 BATCH_SIZE = 1000
+
+def send_to_telegram(chat,message):
+	try:
+		print('Telegram:',message)
+		headers = {
+			"Origin": "http://scriptlab.net",
+			"Referer": "http://scriptlab.net/telegram/bots/relaybot/",
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
+			}
+		url     = "http://scriptlab.net/telegram/bots/relaybot/relaylocked.php?chat="+chat+"&text="+urllib.parse.quote_plus(message)
+		return requests.get(url,headers = headers)
+	except Exception as e:
+		return str(e)
 
 def update_record(server_object, df):
 	
@@ -36,23 +51,26 @@ line = 0
 
 while True:
 	
-	query = """
-		select top """+str(BATCH_SIZE)+""" 
-		id,	text, sentiment
-		from transcribations 
-		where sentiment is NULL and text!=''
-		order by transcribation_date, start
-		"""
+	try:
+		query = """
+			select top """+str(BATCH_SIZE)+""" 
+			id,	text, sentiment
+			from transcribations 
+			where sentiment is NULL and text!=''
+			order by transcribation_date, start
+			"""
 
-	df = pd.read_sql(query, server_object.conn)
-	if len(df)==0:
-		print(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'sleeping 10s..')
-		time.sleep(10)
-	else:
-		print( 'solving '+str(len(df))+' records' )
+		df = pd.read_sql(query, server_object.conn)
+		if len(df)==0:
+			print(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'sleeping 10s..')
+			time.sleep(10)
+		else:
+			print( 'solving '+str(len(df))+' records' )
 
-		#TODO: move model over the cycle (test)
-		model = build_model(configs.classifiers.rusentiment_bert, download=False) #download first time
-		df['sentiment'] = model(df.text)
+			#TODO: move model over the cycle (test)
+			model = build_model(configs.classifiers.rusentiment_bert, download=False) #download first time
+			df['sentiment'] = model(df.text)
 
-		update_record(server_object, df)
+			update_record(server_object, df)
+	except Exception as e:
+		send_to_telegram('106129214',str(datetime.datetime.now())+' stt sentiment error: '+str(e))
