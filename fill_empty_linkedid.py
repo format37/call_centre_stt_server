a03653234f8b4a200e9c4039e7adc0b17c1b3ce6
Alex <format37@gmail.com>
2020-11-23 09:22:40 +0300

    modified:   fill_empty_linkedid.py

--- fill_empty_linkedid.py
+++ fill_empty_linkedid.py
@@ -20,20 +20,24 @@ with open(self.script_path+'mysql.pass','r') as file:
 ms_sql_conn	= connect_sql()
 my_sql_conn	= connect_mysql()
 
-filename = ''
-# read filename
+file_name = ''
+# read filename from transcribations
 with ms_sql_conn:
 	query = 'select top 1 audio_file_name, date_y, date_m, date_d from transcribations where text!='' and linkedid is null and audio_file_name!='' order by date_y,date_m,date_d,transcribation_date;'
 	cursor = ms_sql_conn.cursor()
 	cursor.execute(query)
 	for row in cursor.fetchall():
-		filename = row[0]
+		file_name	= row[0]
+		date_y		= row[1]
+		date_m		= row[2]
+		date_d		= row[3]
 
-if filename=='':
+if file_name=='':
 	print('empty filename. exit')
 	exit()
 
-# read linkedid
+# read linkedid from CDR
+linkedid = ''
 filename = file_name.replace('rxtx.wav','')
 date_from = datetime.datetime(int(self.date_y),int(self.date_m),int(self.date_d))
 date_toto = date_from+datetime.timedelta(days=1)
@@ -41,29 +45,28 @@ date_from = datetime.datetime.strptime(str(date_from), '%Y-%m-%d %H:%M:%S').strf
 date_toto = datetime.datetime.strptime(str(date_toto), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S')
 with my_sql_conn:
 	cursor = my_sql_conn.cursor()
-
-### continue there..
-	query = """
-	select
+	query = """select
 		linkedid
 		from PT1C_cdr_MICO as PT1C_cdr_MICO
 		where 
-			calldate>'"""+date_from+"""' and 
-			calldate<'"""+date_toto+"""' and 
-			PT1C_cdr_MICO.recordingfile LIKE '%"""+filename+"""%' 
-			limit 1
-	"""
+		calldate>'"""+date_from+"""' and 
+		calldate<'"""+date_toto+"""' and 
+		PT1C_cdr_MICO.recordingfile LIKE '%"""+filename+"""%' 
+		limit 1"""
 
 	cursor = self.mysql_conn.cursor()
 	cursor.execute(query)
 	for row in cursor.fetchall():
-		return row[0]
-		
+		linkedid = row[0]
+if linkedid=='':
+	print('linkedid for '+filename+' is empty. exit')
+	exit()		
 
-# save
+# update transcribations
 with my_sql_conn:
 	cursor = my_sql_conn.cursor()
-	sql_query = "insert into transcribations (audio_file_name, transcribation_date, date_y, date_m, date_d, text, start, end_time, side, conf, linkedid) "
-	sql_query += "values ('"+self.original_file_name+"','"+transcribation_date+"','"+self.date_y+"','"+self.date_m+"','"+self.date_d+"','"+accept_text+"','"+str(accept_start)+"','"+str(accept_end)+"',"+str(side)+","+str(conf_mid)+","+str(linkedid)+");"
-	cursor.execute(sql_query)
-	self.conn.commit()
+	query = 'update transcribations set linkedid = '+linkedid+' where audio_file_name='+file_name+';'
+	print(query)
+	#cursor.execute(sql_query)
+	#self.conn.commit()
+	
