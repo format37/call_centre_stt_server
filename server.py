6078be1f34311be0e30bbca04ea8007472edce22
Alex format37 <format37@gmail.com>
2020-12-01 19:18:41 +0300

    modified:   daemon/edit_0.sh 	modified:   daemon/edit_1.sh 	modified:   daemon/edit_2.sh 	modified:   daemon/edit_queue.sh 	modified:   daemon/edit_sentiment.sh 	modified:   daemon/edit_sentiment.txt 	modified:   daemon/enable.sh 	modified:   daemon/reload.sh 	modified:   daemon/restart.sh 	modified:   daemon/stop.sh 	modified:   server.py

--- server.py
+++ server.py
@@ -5,14 +5,18 @@ from init_server import stt_server
 
 server_object = stt_server(sys.argv[1])
 cursor = server_object.conn.cursor()
-sql_query =		"select filepath, filename, date_y, date_m, date_d, duration from queue where cpu_id='"+server_object.cpu_id+"' order by date;"
-
-while True:
+sql_query =		"select top 1 filepath, filename, date_y, date_m, date_d, duration from queue where cpu_id='"+server_object.cpu_id+"' order by date;"
+#last_file_name = ''
+#while True:
+if True:
 	cursor.execute(sql_query)
 	for row in cursor.fetchall():
 
 		server_object.original_file_path	= row[0]
 		server_object.original_file_name	= row[1]
+		#if last_file_name != '' and last_file_name != server_object.original_file_name:
+		#	print(server_object.cpu_id,'next filename. exit')
+		#last_file_name = server_object.original_file_name
 		server_object.date_y				= row[2]
 		server_object.date_m				= row[3]
 		server_object.date_d				= row[4]
@@ -44,6 +48,6 @@ while True:
 			server_object.save_result('', '0', '0', 1, trans_date, 0, linkedid)
 			
 		server_object.delete_current_queue()
-			
-	print(server_object.cpu_id,datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'sleeping 10s..')
-	time.sleep(10)
+	# last_file_name = ''
+	print(server_object.cpu_id,datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'exit to next job..')
+	#time.sleep(10)
