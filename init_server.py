00cee23919446a3496321c56624fcc222ab50519
Alex <format37@gmail.com>
2021-01-11 14:53:08 +0300

    modified:   init_server.py

--- init_server.py
+++ init_server.py
@@ -312,12 +312,11 @@ class stt_server:
 		crop_date_m = crop_date.strftime("%m")
 		crop_date_d = crop_date.strftime("%d")
 
-		cursor = self.conn.cursor()
-		sql_query = """select COUNT(id) 
-		from transcribations 
-		where date_y<='"""+crop_date_y+"""'
-		and date_m<='"""+crop_date_m+"""'
-		and date_d<='"""+crop_date_d+"""';"""
-		print('query',sql_query) # DEBUG
+		# cursor = self.conn.cursor()
+		sql_query = "delete from transcribations where \
+		date_y<='" + crop_date_y + "' and \
+		date_m<='" + crop_date_m + "' and \
+		date_d<='" + crop_date_d + "';"
+		print('query', sql_query) # DEBUG
 		# cursor.execute(sql_query)
 		# self.conn.commit()
