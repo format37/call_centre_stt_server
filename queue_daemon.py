25c3b81024f34c1981c273c386d9c89816e96f81
Alex <format37@gmail.com>
2021-02-18 12:47:11 +0300

    modified:   maintance.ipynb 	modified:   queue_daemon.py

--- queue_daemon.py
+++ queue_daemon.py
@@ -1,50 +1,26 @@
-# call centre queue
 import sys
 import datetime
 import time
 from init_server import stt_server
 server_object = stt_server(0)
 
-"""if len(sys.argv)==4:
-	server_object.date_y = sys.argv[1]
-	server_object.date_m = sys.argv[2]
-	server_object.date_d = sys.argv[3]
-else:
-	server_object.set_today_ymd()
-"""
-
 while True:
 	for source_id in server_object.sources: # ['call', 'master']
 
 		server_object.source_id = server_object.get_source_id(source_id)
-		#if server_object.source_id == 1: # ToDo: REMOVE when stt mrm ready
-		#	continue
 		complete_files	= server_object.get_sql_complete_files()
 		incomplete_count = 0
-		# complete_count = 0
 		for filepath, filename, rec_date, src, dst, linkedid in server_object.get_fs_files_list(complete_files):
-			#server_object.rec_date = rec_date
-			#if not filename in complete_files:
-			#server_object.original_file_name = filename
-			#server_object.src = src
-			#server_object.dst = dst
-			#server_object.linkedid = linkedid
 			server_object.set_shortest_queue_cpu()
 			server_object.add_queue(filepath, filename, rec_date, src, dst, linkedid)
-			# print('id', source_id, 'incomplete', incomplete_count, 'cpu', server_object.cpu_id, 'file', filename)
 			incomplete_count += 1
-			"""else:
-				complete_count += 1"""
-			#break # ToDo: remove
 
 		print('id', source_id)
 		print(incomplete_count, 'files sent to queue', server_object.sources[source_id])
-		# print('complete_count',complete_count)
-	
-	"""if len(sys.argv) == 4:
-		break
-	else:"""
-	print(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'sleeping 10s..')
-	time.sleep(10)
-	#server_object.set_today_ymd()
-	break # ToDo: remove
+
+	sleep_time = 600
+	print(
+		datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+		'sleeping '+str(sleep_time)+'s..'
+	)
+	time.sleep(sleep_time)
