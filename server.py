78e9546e1a24e9f45b8d4cb2caa081344b2e3287
Alex <format37@gmail.com>
2021-03-03 17:20:23 +0300

    modified:   maintance.ipynb 	modified:   server.py

--- server.py
+++ server.py
@@ -8,10 +8,9 @@ import os
 print('cpu', sys.argv[1])
 server_object = stt_server(sys.argv[1])
 cursor = server_object.conn.cursor()
-sql_query = "select filepath, filename, duration, source_id, "
 sql_query += "record_date, src, dst, linkedid from queue "
 sql_query += "where cpu_id='"+str(server_object.cpu_id)+"' "
-sql_query += "and source_id = '2' " # debug
+#sql_query += "and source_id = '2' " # debug
 sql_query += "order by record_date, linkedid, filename;"
 processed = 0
 cursor.execute(sql_query)
