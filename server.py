3e392d961cece5196a33a224c377088d2ee2cc64
Alex <format37@gmail.com>
2021-03-05 15:29:04 +0300

    modified:   server.py

--- server.py
+++ server.py
@@ -103,7 +103,7 @@ for row in cursor.fetchall():
 
 	else:
 		print(original_file_name, 'duration', original_file_duration)
-		trans_date = datetime.datetime.now()
+		trans_date = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 		if server_object.source_id == server_object.sources['master']:
 			side = 0 if 'in' in original_file_name else 1
 			server_object.save_result(
