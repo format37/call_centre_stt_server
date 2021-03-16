30b75faa214b1379f141bfb0df42f041017b7164
Alex <format37@gmail.com>
2021-03-16 10:59:48 +0300

    modified:   init_server.py 	modified:   maintance.ipynb 	modified:   performance_report.ipynb

--- init_server.py
+++ init_server.py
@@ -19,7 +19,7 @@ class stt_server:
 
 		# settings ++
 		self.cpu_id = cpu_id
-		self.cpu_cores = [i for i in range(0,17)]
+		self.cpu_cores = [i for i in range(0,2)]
 
 		# telegram
 		#self.telegram_chat = '106129214' # alex
