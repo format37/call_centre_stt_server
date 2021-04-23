3851dcb93dd2da9b71d45db59e53a2dd12eb0564
Alex <format37@gmail.com>
2021-04-23 11:00:03 +0300

    modified:   docker/mysql.ipynb 	modified:   monitoring/server.py

--- monitoring/server.py
+++ monitoring/server.py
@@ -4,6 +4,7 @@ from aiohttp import web
 import uuid
 import pandas as pd
 from os import unlink
+import pymysql
 PORT = '8083'
 
 async def call_test(request):
@@ -12,20 +13,37 @@ async def call_test(request):
         text='ok',
         content_type="text/html")
 
-async def call_compute(request):
+async def call_log(request):
+
     print('call_compute')
-    # save data
+    # data -> df
     filename = str(uuid.uuid4())+'.csv'
     with open(filename, 'w') as source_file:
         source_file.write(await request.text())
         source_file.close()
     df = pd.read_csv(filename, ';', dtype={'id': 'int', 'name': 'str'})
     unlink(filename)
-    df.id += 10
-    df.name = 'hello from python: '+df.name
+
+    # df -> mysql
+    with open('mysql_local.pass', 'r') as file:
+        mysql_pass = file.read().replace('\n', '')
+        file.close()
+    conn = pymysql.connect(
+        host='10.2.4.87',
+        user='root',
+        passwd=mysql_pass,
+        db='1c',
+        autocommit=True
+    )
+    cursor = conn.cursor()
+    query = "show tables;"
+    cursor.execute(query)
+    for row in cursor.fetchall():
+        print(row)
+
     answer = 'list:'
-    for nom_id, nom_name in df.values:
-        answer += '\n' + str(nom_id) + ';'+nom_name
+    #for nom_id, nom_name in df.values:
+    #    answer += '\n' + str(nom_id) + ';'+nom_name
 
     return web.Response(
         text=answer,
@@ -33,7 +51,7 @@ async def call_compute(request):
 
 app = web.Application()
 app.router.add_route('GET', '/test', call_test)
-app.router.add_route('POST', '/compute', call_compute)
+app.router.add_route('POST', '/log', call_log)
 
 loop = asyncio.get_event_loop()
 handler = app.make_handler()
