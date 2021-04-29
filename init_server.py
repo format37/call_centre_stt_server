54f1972781cf0c65a319758599c068142ef61a14
Alex <format37@gmail.com>
2021-04-29 16:01:11 +0300

    modified:   init_server.py 	modified:   maintance.ipynb

--- init_server.py
+++ init_server.py
@@ -576,13 +576,20 @@ class stt_server:
 
 	def add_queue(self, filepath, filename, rec_date, src, dst, linkedid, naming_version):
 
-		file_stat = os.stat(filepath + filename)
+		try:
+			file_stat = os.stat(filepath + filename)
+			f_size = file_stat.st_size
+		except Exception as e:
+			f_size = -1
+			print('file stat error:', str(e))
+			self.send_to_telegram(str(e))
+
 		if time.time() - file_stat.st_mtime > 60:
 			file_duration = self.calculate_file_length(filepath, filename)
 
 			if file_duration == 0:
 				message = 'zero file in queue: t[' + str(time.time() - file_stat.st_mtime) + ']  '
-				message += 's[' + str(file_stat.st_size) + ']  '
+				message += 's[' + str(f_size) + ']  '
 				message += 'd[' + str(file_duration) + ']  '
 				message += str(filename)
 				# self.save_file_for_analysis(filepath, filename, file_duration)
