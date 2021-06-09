963f3724af9fec470f50f614db370579bcad6184
format37 <format37@gmail.com>
2021-06-09 09:07:58 +0300

    modified:   summarization.py 	modified:   summarizator.ipynb

--- summarization.py
+++ summarization.py
@@ -79,34 +79,40 @@ def sum_to_sql(linkedid, recor_date, side, text, phrases_count, text_length):
     cursor.execute(query)
 
 
-while True:
 
-    # obtain datetime limits
-    query = "select linkedid from queue;"
-    df = read_sql(query)
-    if len(df):
-        query = "select min(record_date) from queue where not isnull(record_date,'')='';"
-        df = read_sql(query)
-        queue_first_record = str(df.iloc()[0][0])
-    else:
-        queue_first_record = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    query = "select max(record_date) from summarization;"
-    df = read_sql(query)
-    summarization_first_record = str(df.iloc()[0][0])
 
-    # concatenate transcribations
-    query = "SELECT distinct top "+str(BATCH_SIZE)+" record_date, linkedid"
-    query += " from transcribations"
-    query += " where "
-    query += " record_date < '"+queue_first_record+"' and"
-    query += " not linkedid in (select distinct linkedid from summarization)"
-    query += " order by record_date desc;"
+# obtain datetime limits
+query = "select linkedid from queue;"
+df = read_sql(query)
+if len(df):
+    query = "select min(record_date) from queue where not isnull(record_date,'')='';"
     df = read_sql(query)
+    queue_first_record = str(df.iloc()[0][0])
+else:
+    queue_first_record = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+query = "select max(record_date) from summarization;"
+df = read_sql(query)
+summarization_first_record = str(df.iloc()[0][0])
 
-    for _id, row in df.iterrows():
-        
-        for side in range(2):
-            text_full, phrases_count = concatenate_linkedid_side(side, row.record_date, row.linkedid)
-            text_short = summarize(text_full, phrases_count)
-            sum_to_sql(row.linkedid, row.record_date, side, text_short, phrases_count, len(text_full))
-    break # TODO: remove
+# concatenate transcribations
+query = "SELECT distinct top "+str(BATCH_SIZE)+" record_date, linkedid"
+query += " from transcribations"
+query += " where "
+query += " record_date < '"+queue_first_record+"' and"
+query += " not linkedid in (select distinct linkedid from summarization)"
+query += " order by record_date desc;"
+df = read_sql(query)
+
+for _id, row in df.iterrows():
+    
+    for side in range(2):
+        text_full, phrases_count = concatenate_linkedid_side(side, row.record_date, row.linkedid)
+        text_short = summarize(text_full, phrases_count)
+        sum_to_sql(row.linkedid, row.record_date, side, text_short, phrases_count, len(text_full))
+
+print(
+    datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+    'summarized:', len(df),
+    'from:', min(df.record_date),
+    'to:', max(df.record_date)
+    )
