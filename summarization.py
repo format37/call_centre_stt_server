bcc747137c3a2678ea1bc6f66ed1903fdbb49057
format37 <format37@gmail.com>
2021-06-18 15:25:43 +0300

    modified:   summarization.py

--- summarization.py
+++ summarization.py
@@ -91,9 +91,9 @@ def get_jaccard_sim(str1, str2):
     a = set(str1.split()) 
     b = set(str2.split())
     c = a.intersection(b)
-    bottom = (len(a) + len(b) - len(c)
-    if bottom>0:
-        return float(len(c)) / bottom)
+    denominator = (len(a) + len(b) - len(c)
+    if denominator>0:
+        return float(len(c)) / denominator)
     else:
         return 0
 
