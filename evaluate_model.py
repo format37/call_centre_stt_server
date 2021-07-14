43283a0c57b6102c3c91fbba4f94d4ef6489a47c
format37 <format37@gmail.com>
2021-07-14 07:58:17 +0300

    modified:   evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -125,7 +125,7 @@ def error(ground_truth, hypothesis):
     measures = jiwer.compute_measures(ground_truth, hypothesis)
     return measures
 
-def send_report(evaluation, script_path, tg_group, description):
+def send_report(bot, evaluation, script_path, tg_group, description):
 
     mycolors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:brown', 'tab:gray']
     fig, ax = plt.subplots(1,1,figsize=(16, 9), dpi= 80)
@@ -137,85 +137,95 @@ def send_report(evaluation, script_path, tg_group, description):
     plt.legend()
     plt.savefig(script_path+'evaluation.png')
 
-    with open(script_path+'telegram_token.key', 'r') as file:
-        token = file.read().replace('\n', '')
-        file.close()
-    bot = telebot.TeleBot(token)
     with open(script_path+'evaluation.png', 'rb') as data_file:
         print('sending photo to ', tg_group)
         bot.send_photo(tg_group, data_file)
 
-files = get_files(file_path)
-param_date = sys.argv[1]
-if param_date =='default':
-    current_date = (datetime.datetime.now() + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
-else:
-    current_date = sys.argv[1]
-evals_wer = []
-evals_mer = []
-evals_wil = []
 
-evaluation_file = script_path + 'evaluation.csv'
-evaluation = pd.read_csv(evaluation_file, parse_dates = False)
+with open(script_path+'telegram_token.key', 'r') as file:
+    token = file.read().replace('\n', '')
+bot = telebot.TeleBot(token)
 
-for file in files:
-    if current_date+'_' in file:
-        print('file:', file)
-        text_google = transcribe_google(file_path+file).replace('  ',' ')
-        if len(text_google)<10:
-            print('break google len', len(text_google))
+
+bot.send_message(tg_group, str(datetime.datetime.now())+' evaluation report started')
+
+try:
+    files = get_files(file_path)
+    param_date = sys.argv[1]
+    if param_date =='default':
+        current_date = (datetime.datetime.now() + datetime.timedelta(days=-1)).strftime('%Y-%m-%d')
+    else:
+        current_date = sys.argv[1]
+    evals_wer = []
+    evals_mer = []
+    evals_wil = []
+
+    evaluation_file = script_path + 'evaluation.csv'
+    evaluation = pd.read_csv(evaluation_file, parse_dates = False)
+
+    for file in files:
+        if current_date+'_' in file:
+            print('file:', file)
+            text_google = transcribe_google(file_path+file).replace('  ',' ')
+            if len(text_google)<10:
+                print('break google len', len(text_google))
+                os.unlink(file_path+file)
+                continue
+            if string_have_numbers(text_google):
+                print('break google numbers:', text_google)
+                os.unlink(file_path+file)
+                continue
+            print('google:', text_google)
+            text_vosk = ' '.join(transcribe_vosk(file_path+file, model_path)).replace('  ',' ')
+            if len(text_vosk)<10:
+                print('break vosk len', len(text_vosk))
+                os.unlink(file_path+file)
+                continue
+            print('vosk:', text_vosk)
+            measures = error(text_google, text_vosk)
+            print(measures)
+            evals_wer.append(measures['wer'])
+            evals_mer.append(measures['mer'])
+            evals_wil.append(measures['wil'])
             os.unlink(file_path+file)
-            continue
-        if string_have_numbers(text_google):
-            print('break google numbers:', text_google)
-            os.unlink(file_path+file)
-            continue
-        print('google:', text_google)
-        text_vosk = ' '.join(transcribe_vosk(file_path+file, model_path)).replace('  ',' ')
-        if len(text_vosk)<10:
-            print('break vosk len', len(text_vosk))
-            os.unlink(file_path+file)
-            continue
-        print('vosk:', text_vosk)
-        measures = error(text_google, text_vosk)
-        print(measures)
-        evals_wer.append(measures['wer'])
-        evals_mer.append(measures['mer'])
-        evals_wil.append(measures['wil'])
-        os.unlink(file_path+file)
 
-if len(evals_wer) + len(evals_mer) + len(evals_wil) > 0:
+    if len(evals_wer) + len(evals_mer) + len(evals_wil) > 0:
 
-    print('avg: wer', np.average(evals_wer), 'mer', np.average(evals_mer), 'wil', np.average(evals_wil))
-    print('med: wer', np.median(evals_wer), 'mer', np.median(evals_mer), 'wil', np.median(evals_wil))
+        print('avg: wer', np.average(evals_wer), 'mer', np.average(evals_mer), 'wil', np.average(evals_wil))
+        print('med: wer', np.median(evals_wer), 'mer', np.median(evals_mer), 'wil', np.median(evals_wil))
 
-    current = pd.DataFrame(columns = ['date', 'avg_wil', 'avg_wer', 'avg_mer', 'med_wil', 'med_wer', 'med_mer'])
-    current['date'] = pd.to_datetime(current_date).date()
-    current['avg_wil'] = [np.average(evals_wil)]
-    current['avg_wer'] = [np.average(evals_wer)]
-    current['avg_mer'] = [np.average(evals_mer)]
-    current['med_wil'] = [np.median(evals_wil)]
-    current['med_wer'] = [np.median(evals_wer)]
-    current['med_mer'] = [np.median(evals_mer)]
+        current = pd.DataFrame(columns = ['date', 'avg_wil', 'avg_wer', 'avg_mer', 'med_wil', 'med_wer', 'med_mer'])
+        current['date'] = pd.to_datetime(current_date).date()
+        current['avg_wil'] = [np.average(evals_wil)]
+        current['avg_wer'] = [np.average(evals_wer)]
+        current['avg_mer'] = [np.average(evals_mer)]
+        current['med_wil'] = [np.median(evals_wil)]
+        current['med_wer'] = [np.median(evals_wer)]
+        current['med_mer'] = [np.median(evals_mer)]
 
-    row = dict()
-    row['date'] = pd.to_datetime(current_date).date()
-    row['avg_wil'] = np.average(evals_wil)
-    row['avg_wer'] = np.average(evals_wer)
-    row['avg_mer'] = np.average(evals_mer)
-    row['med_wil'] = np.median(evals_wil)
-    row['med_wer'] = np.median(evals_wer)
-    row['med_mer'] = np.median(evals_mer)
-    current  = pd.DataFrame([row], columns=row.keys())
+        row = dict()
+        row['date'] = pd.to_datetime(current_date).date()
+        row['avg_wil'] = np.average(evals_wil)
+        row['avg_wer'] = np.average(evals_wer)
+        row['avg_mer'] = np.average(evals_mer)
+        row['med_wil'] = np.median(evals_wil)
+        row['med_wer'] = np.median(evals_wer)
+        row['med_mer'] = np.median(evals_mer)
+        current  = pd.DataFrame([row], columns=row.keys())
 
-    evaluation = pd.concat([evaluation, current], axis = 0)
-    evaluation.to_csv(evaluation_file, index = False)
+        evaluation = pd.concat([evaluation, current], axis = 0)
+        evaluation.to_csv(evaluation_file, index = False)
 
-start_date = pd.to_datetime((datetime.datetime.now() + datetime.timedelta(days=-10)).strftime('%Y-%m-%d'))
-evaluation.date = pd.to_datetime(evaluation.date)
-evaluation = pd.DataFrame(evaluation[evaluation.date>start_date])
+    start_date = pd.to_datetime((datetime.datetime.now() + datetime.timedelta(days=-10)).strftime('%Y-%m-%d'))
+    evaluation.date = pd.to_datetime(evaluation.date)
+    evaluation = pd.DataFrame(evaluation[evaluation.date>start_date])
 
-send_report(evaluation.drop(['med_wil', 'med_wer', 'med_mer'], 1), script_path, tg_group, 'average')
-send_report(evaluation.drop(['avg_wil', 'avg_wer', 'avg_mer'], 1), script_path, tg_group, 'median')
+    send_report(bot, evaluation.drop(['med_wil', 'med_wer', 'med_mer'], 1), script_path, tg_group, 'average')
+    send_report(bot, evaluation.drop(['avg_wil', 'avg_wer', 'avg_mer'], 1), script_path, tg_group, 'median')
+
+except Exception as e:
+    print('error:', str(e))
+    bot.send_message(tg_group, str(datetime.datetime.now())+' evaluation report error: '+str(e))
 
 print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'job complete')
+bot.send_message(tg_group, str(datetime.datetime.now())+' evaluation report complete')
