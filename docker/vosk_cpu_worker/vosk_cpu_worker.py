78ac8de5247104e62e5b95203e71b9f965159454
alex <format37@gmail.com>
2021-09-03 12:26:19 +0300

    modified:   docker/docker-compose.yml 	modified:   docker/vosk_cpu_worker/vosk_cpu_worker.py

--- docker/vosk_cpu_worker/vosk_cpu_worker.py
+++ docker/vosk_cpu_worker/vosk_cpu_worker.py
@@ -2,15 +2,14 @@ import time
 import os
 
 i=0
-"""for root, dirs, files in os.walk(self.original_storage_path[self.source_id]):
+for root, dirs, files in os.walk('audio/mono'):
 	for filename in files:
 		print(root, filename)
 		if i>10:
 			break
-"""
 print('files')
 #print('mssql')
 #print('mysql')
-while True:
-	time.sleep(3)
+#while True:
+time.sleep(3)
 print('normal exit')
