93c0a8fce11c8d07675c5e597466075682cad556
alex <format37@gmail.com>
2021-09-06 13:09:13 +0300

    modified:   vosk_cpu_worker/vosk_cpu_worker.py 	modified:   vosk_queue/queue.py 	modified:   ../maintance.ipynb

--- docker/vosk_queue/queue.py
+++ docker/vosk_queue/queue.py
@@ -2,6 +2,10 @@ import datetime
 import time
 from init_queue import stt_server
 
+exit()
+
+batch_size = 1000
+
 server_object = stt_server()
 
 for source_id in server_object.sources: # ['call', 'master']
@@ -14,13 +18,13 @@ for source_id in server_object.sources: # ['call', 'master']
 		server_object.set_shortest_queue_cpu()
 		server_object.add_queue(filepath, filename, rec_date, src, dst, linkedid, version)
 		incomplete_count += 1
-		print(incomplete_count)
+		if incomplete_count>batch_size
+			print('batch size reached. break')
 
 	print('id', source_id)
 	print(incomplete_count, 'files sent to queue', server_object.sources[source_id])
 
 sleep_time = 3
-#while True:
 print(
 	datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
 	'sleeping '+str(sleep_time)+'s..'
