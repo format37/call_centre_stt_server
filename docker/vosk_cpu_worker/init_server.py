f07f9e84155cbb186b4d3f56dff2f1b4ac027a9e
alex <format37@gmail.com>
2021-09-27 13:38:04 +0300

    modified:   docker/vosk_cpu_worker/init_server.py

--- docker/vosk_cpu_worker/init_server.py
+++ docker/vosk_cpu_worker/init_server.py
@@ -346,6 +346,32 @@ class stt_server:
 
 		return phrases_count, phrases, confidences
 
+
+	def phrases_have_numbers(phrases):
+		numbers = [
+			'надцать',
+			'двадцать',
+			'тридцать',
+			'сорок',
+			'пятьдесят',
+			'шестьдесят',
+			'семьдесят',
+			'восемьдесят',
+			'девяносто',
+			'пятьсот',
+			'шестьсот',
+			'семьсот',
+			'восемьсот',
+			'девятьсот',
+			'тысяч'
+		]
+		for word in ' '.join(phrases).split(' '):
+			if word in numbers:
+				return True
+		return False
+		
+
+
 	def transcribe_to_sql(
 		self, 
 		duration, 
@@ -394,6 +420,7 @@ class stt_server:
 			self.confidence_of_file>0.5 and \
 			duration > 50 and \
 			duration < 60 and \
+			not phrases_have_numbers(phrases) and \
 			not self.wer_file_exist():
 			self.save_file_for_analysis(self.temp_file_path, self.temp_file_name, duration)
 			#self.send_to_telegram(str(self.cpu_id)+': '+str(phrases_count)+' # '+self.temp_file_name)
