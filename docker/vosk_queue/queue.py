729799b3f20feb0ab89fc7b292d46451c58475a8
alex <format37@gmail.com>
2021-12-27 14:12:31 +0300

    modified:   docker/vosk_queue/queue.py

--- docker/vosk_queue/queue.py
+++ docker/vosk_queue/queue.py
@@ -8,20 +8,20 @@ server_object = stt_server()
 
 for source_id in server_object.sources: # ['call', 'master']
 
-	if source_id == 'call': # debug
-		server_object.source_id = server_object.get_source_id(source_id)
-		complete_files = server_object.get_sql_complete_files()
-		incomplete_count = 0
-		for filepath, filename, rec_date, src, dst, linkedid, version in server_object.get_fs_files_list(complete_files):
-			server_object.set_shortest_queue_cpu()
-			server_object.add_queue(filepath, filename, rec_date, src, dst, linkedid, version)
-			incomplete_count += 1
-			if incomplete_count>batch_size:
-				print('batch size reached. break')
-				break
+	#if source_id == 'call': # debug
+	server_object.source_id = server_object.get_source_id(source_id)
+	complete_files = server_object.get_sql_complete_files()
+	incomplete_count = 0
+	for filepath, filename, rec_date, src, dst, linkedid, version in server_object.get_fs_files_list(complete_files):
+		server_object.set_shortest_queue_cpu()
+		server_object.add_queue(filepath, filename, rec_date, src, dst, linkedid, version)
+		incomplete_count += 1
+		if incomplete_count>batch_size:
+			print('batch size reached. break')
+			break
 
-		print('id', source_id)
-		print(incomplete_count, 'files sent to queue', server_object.sources[source_id])
+	print('id', source_id)
+	print(incomplete_count, 'files sent to queue', server_object.sources[source_id])
 
 sleep_time = 3
 print(
