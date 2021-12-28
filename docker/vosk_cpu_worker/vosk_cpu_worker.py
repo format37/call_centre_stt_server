c67f186e55a71959243a0bec8e722d356d733ac8
alex <format37@gmail.com>
2021-12-28 08:25:37 +0300

    modified:   docker/vosk_cpu_worker/vosk_cpu_worker.py

--- docker/vosk_cpu_worker/vosk_cpu_worker.py
+++ docker/vosk_cpu_worker/vosk_cpu_worker.py
@@ -2,8 +2,6 @@ import os
 import time
 import datetime
 import pendulum
-#import sys
-#sys.path.insert(0,'../')
 from init_server import stt_server
 
 server_object = stt_server()
@@ -18,9 +16,8 @@ while True:
 	sql_query = "select filepath, filename, duration, source_id, "
 	sql_query += "record_date, src, dst, linkedid, file_size, date from queue "
 	sql_query += "where cpu_id='" + str(server_object.cpu_id) + "' "
-	#sql_query += "and ( (source_id = '2' and record_date < '" + past_in_minutes + "') or not source_id = '2' ) "
 	sql_query += "and record_date < '" + past_in_minutes + "' "
-	sql_query += "order by record_date, filename;"
+	sql_query += "order by record_date desc, filename;"
 	processed = 0
 	cursor.execute(sql_query)
 	linkedid = ''
@@ -55,144 +52,50 @@ while True:
 
 		side = 0 if original_file_name[-6:]=='in.wav' else 1
 
-		if original_file_duration>5:
-
-			#if server_object.source_id == server_object.sources['master']:
-			if True:
-				server_object.temp_file_path = original_file_path
-				server_object.temp_file_name = original_file_name
-				if os.path.isfile(server_object.temp_file_path + server_object.temp_file_name):
-					#side = 0 if 'in' in original_file_name else 1
-					#side = 0 if original_file_name[-6:]=='in.wav' else 1
-					server_object.transcribe_to_sql(
-						original_file_duration,
-						side,
-						original_file_name,
-						rec_date,
-						src,
-						dst,
-						linkedid,
-						file_size,
-						queue_date
-					)
-					files_converted += 1				
-				else:
-					print(
-						'id: '+str(server_object.source_id)+' file not found', 
-						server_object.temp_file_path + server_object.temp_file_name
-						)
-				
-				server_object.delete_current_queue(original_file_name, linkedid)
-
-
-			"""elif server_object.source_id == server_object.sources['call']:
-
-				if server_object.make_file_splitted(
-						0,
-						original_file_path,
-						original_file_name,
-						linkedid,
-						original_file_duration
-				):
-					server_object.transcribe_to_sql(
-						original_file_duration,
-						0,
-						original_file_name,
-						rec_date,
-						src,
-						dst,
-						linkedid,
-						file_size,
-						queue_date
-					)
-					server_object.remove_temporary_file()
-					files_converted += 1
-
-				if server_object.make_file_splitted(
-						1,
-						original_file_path,
-						original_file_name,
-						linkedid,
-						original_file_duration
-				):
-					server_object.transcribe_to_sql(
-						original_file_duration,
-						1,
-						original_file_name,
-						rec_date,
-						src,
-						dst,
-						linkedid,
-						file_size,
-						queue_date
-					)
-					server_object.remove_temporary_file()
-					files_converted += 1
-
-				if files_converted == 0:
-					print('file not found:', linkedid, 'removing from queue..')
-					server_object.delete_current_queue(original_file_name, linkedid)"""
-
-			print('files_converted', files_converted)		
-			
+		if original_file_duration>5:			
+			server_object.temp_file_path = original_file_path
+			server_object.temp_file_name = original_file_name
+			if os.path.isfile(server_object.temp_file_path + server_object.temp_file_name):
+				server_object.transcribe_to_sql(
+					original_file_duration,
+					side,
+					original_file_name,
+					rec_date,
+					src,
+					dst,
+					linkedid,
+					file_size,
+					queue_date
+				)
+				files_converted += 1				
+			else:
+				print(
+					'id: '+str(server_object.source_id)+' file not found', 
+					server_object.temp_file_path + server_object.temp_file_name
+					)				
+			server_object.delete_current_queue(original_file_name, linkedid)
+			print('files_converted', files_converted)			
 			server_object.delete_source_file(original_file_path, original_file_name, linkedid)
 
 		else:
 			print(original_file_name, 'duration', original_file_duration)
 			trans_date = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-			#if server_object.source_id == server_object.sources['master']:
-			if True:
-				#side = 0 if 'in' in original_file_name else 1
-				#side = 0 if original_file_name[-6:]=='in.wav' else 1
-				server_object.save_result(
-					original_file_duration,
-					'',
-					'0',
-					'0',
-					side,
-					trans_date,
-					0,
-					original_file_name,
-					rec_date,
-					src,
-					dst,
-					linkedid,
-					file_size,
-					queue_date
-				)
-			"""elif server_object.source_id == server_object.sources['call']:
-				server_object.save_result(
-					original_file_duration,
-					'',
-					'0',
-					'0',
-					side,
-					trans_date,
-					0,
-					original_file_name,
-					rec_date,
-					src,
-					dst,
-					linkedid,
-					file_size,
-					queue_date
-				)
-				server_object.save_result(
-					original_file_duration,
-					'',
-					'0',
-					'0',
-					1,
-					trans_date,
-					0,
-					original_file_name,
-					rec_date,
-					src,
-					dst,
-					linkedid,
-					file_size,
-					queue_date
-				)"""
+			server_object.save_result(
+				original_file_duration,
+				'',
+				'0',
+				'0',
+				side,
+				trans_date,
+				0,
+				original_file_name,
+				rec_date,
+				src,
+				dst,
+				linkedid,
+				file_size,
+				queue_date
+			)
 
 			server_object.delete_current_queue(original_file_name, linkedid)
 			server_object.delete_source_file(original_file_path, original_file_name, linkedid)
@@ -202,10 +105,7 @@ while True:
 
 		processed += 1
 		print(' === processed:', processed, '/', len(fetched), 'files_converted:', files_converted, '===')
-		"""if processed > batch_size:
-			print('processed > batch_size')
-			break"""
 
 	print(server_object.cpu_id,datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), 'queue reached..')
 	if processed == 0:
-		time.sleep(10)
+		time.sleep(3)
